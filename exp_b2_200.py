a34a14d6bd0b705a994567f25a48958f9d916940
Maggie (Mengqing) ZHANG <maggiezhangdata@gmail.com>
2023-11-13 15:54:00 -0600

    Update exp_b2_200.py

--- exp_b2_200.py
+++ exp_b2_200.py
@@ -3,19 +3,11 @@ import streamlit as st
 import time
 import re  # Import regular expressions
 
-# Hide the footer
-hide_footer_style = """
-    <style>
-        .css-1lcbmhc {display: none;}
-    </style>
-"""
-st.markdown(hide_footer_style, unsafe_allow_html=True)
-
-
 st.title("真正倾听您说话的聊天机器人")
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 assistant_id = st.secrets["assistant_id_b2_200"]
 speed = 200
+
 
 
 if "thread_id" not in st.session_state:
